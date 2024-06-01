017862265516c90ba8acc217467acc13673a1d5a
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-06-01 08:34:58 +0530

    Time: 1 ms (76.09%) | Memory: 45.1 MB (92.85%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,19 +1,24 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
+        int m = matrix.length;
+        int n = matrix[0].length;
         boolean flag = true;
-        for(int i = 0; i < matrix.length; i++){
+
+        for(int i = 0; i < m; i++)
+        {
             if(matrix[i][0] == 0)
                 flag = false;
-            for(int j = 1; j < matrix[0].length; j++){
-                if(matrix[i][j] == 0){
+            for(int j = 1; j < n; j++){
+                if(matrix[i][j] == 0)
+                {
                     matrix[0][j] = 0;
                     matrix[i][0] = 0;
                 }
             }
         }
 
-        for(int i = matrix.length - 1;i >= 0; i--){
-            for(int j = matrix[0].length - 1; j >= 1; j--){
+        for(int i = m - 1;i >= 0; i--){
+            for(int j = n - 1; j >= 1; j--){
                 if(matrix[0][j] == 0 || matrix[i][0] == 0){
                     matrix[i][j] = 0;
                 }
@@ -22,5 +27,6 @@ class Solution {
                 matrix[i][0] = 0;
             }
         }
+
     }
 }
