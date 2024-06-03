bb651c63d482dd9e0291f92a168e95b35893ff9d
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-06-03 10:03:56 +0530

    Time: 19 ms (100.00%) | Memory: 43.2 MB (5.93%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -2,40 +2,41 @@ class Solution {
     public void rotate(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
+        transpose(matrix,m,n);
+        print(matrix,m,n);
+        System.out.println("-------");
+        swapColumns(matrix,m,n);
+        print(matrix,m,n);
+    }
 
-        int k = 0;
-        for(int i = 0; i < m;i++){
-            for(int j = k; j < n; j++){
+    private void transpose(int[][] matrix, int m, int n){
+
+        for(int i = 0; i < m; i++){
+            for(int j = i; j < n; j++){
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
-                //swap(matrix[i][j],matrix[j][i]);
             }
-            k++;
-        }
-
-        for(int i = 0; i <= (m - 1) / 2; i++){
-            int l = i;
-            int r = m - 1 - i;
-            for(int j = 0; j < n; j++){
-                int temp = matrix[j][l];
-                matrix[j][l] = matrix[j][r];
-                matrix[j][r] = temp;
-            }
-        }
-
-        for(int i = 0; i < m;i++){
-            for(int j = 0; j < n; j++){
-                System.out.print(matrix[i][j]+" ");
-            }
-            System.out.println();
-            // k++;
         }
     }
 
-    public void swap(int i, int j){
-        int k = i;
-        i = j;
-        j = k;
+    private void swapColumns(int[][] matrix, int m, int n){
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n / 2; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
+            }
+        }
+    }
+
+    private void print(int[][] matrix, int m, int n){
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
