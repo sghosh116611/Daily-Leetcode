7f3b1ff42576f89eb5848f76c11ac1c55d51bb1e
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-06-22 10:17:04 +0530

    Time: 1 ms (99.86%) | Memory: 48.5 MB (38.04%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.java
@@ -11,44 +11,41 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+
+        ListNode newP1 = headA;
+        ListNode newP2 = headB;
+
         int c1 = 0, c2 = 0;
-        ListNode p = headA;
-        ListNode q = headB;
 
-        ListNode intersection = null;
-
-        while(p != null){
+        while(p1.next != null){
+            p1 = p1.next;
             c1++;
-            p = p.next;
         }
 
-        while(q != null){
+        while(p2.next != null){
+            p2 = p2.next;
             c2++;
-            q = q.next;
         }
 
         if(c1 > c2){
-            intersection = findIntersection(headA,headB,c1-c2);
+            int diff = c1 - c2;
+            while(diff-- > 0){
+                newP1 = newP1.next;
+            }
         }else{
-            intersection = findIntersection(headB,headA,c2-c1);
+            int diff = c2 - c1;
+            while(diff-- > 0){
+                newP2 = newP2.next;
+            }
         }
 
-        return intersection;
-    }
-
-    private ListNode findIntersection(ListNode headA, ListNode headB, int diff){
-        ListNode p = headA, q = headB;
-
-        while(diff != 0 && p != null){
-            p = p.next;
-            diff--;
-        }
-
-        while(p != null && q != null){
-            if(p == q)
-                return p;
-            p = p.next;
-            q = q.next;
+        while(newP1 != null && newP2 != null){
+            if(newP1 == newP2)
+                return newP1;
+            newP1 = newP1.next;
+            newP2 = newP2.next;
         }
 
         return null;
