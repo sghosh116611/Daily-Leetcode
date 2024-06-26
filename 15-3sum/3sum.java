2d4989eac244ce3909eb1649a18d3fbe3fe674cb
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-06-26 09:30:04 +0530

    Time: 33 ms (46.86%) | Memory: 52.6 MB (19.26%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,36 +1,33 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
 
-        Arrays.sort(nums);
-        
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] > 0) // To check for first positive
-                break;
-            if(i > 0 && nums[i] == nums[i-1]) // To identify unique solution for every nums[i]
-                continue;
-            int k = -1 * nums[i];
-            int low = i + 1;
-            int high = nums.length - 1;
-            while(low < high){
-                int sum = nums[low] + nums[high];
-                if(sum == k){
-                    List triplet = new ArrayList<>();
-                    triplet.add(nums[i]);
-                    triplet.add(nums[low]);
-                    triplet.add(nums[high]);
-                    result.add(triplet);
-                    while(low < high && nums[low] == nums[low+1]) // To skip every occurence of duplicate solution
-                        low++;
-                    while(low < high && nums[high] == nums[high-1])
-                        high--;
-                    low++;
-                    high--;
-                }else if(sum < k){
-                    low++;
-                }else
-                    high--;
+        for(int i = 0; i < nums.length - 1; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+            int p = i + 1;
+            int q = nums.length - 1;
+            
+            while(p < q){
+                int sum = nums[i] + nums[p] + nums[q];
+                if(sum == 0){
+                    List<Integer> res = new ArrayList<>();
+                    res.add(nums[i]);
+                    res.add(nums[p]);
+                    res.add(nums[q]);
+                    result.add(res);
+                    while(p < q && nums[p] == nums[p + 1]) p++;
+                    while(p < q && nums[q] == nums[q - 1]) q--;
+                    p++;
+                    q--;
+                }else if(sum > 0){
+                    q--;
+                }else{
+                    p++;
+                }
             }
+
+            
         }
 
         return result;
