4c5e4a453f03fccb189e9f56fdb2f39fec4e1390
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-07-11 18:20:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (79.92%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,35 +1,33 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int n = nums.length;
-        int br = -1;
-        for(int i = n - 2; i >= 0; i--){
-            if(nums[i] < nums[ i + 1 ]){
-                br = i;
+        
+        int point = nums.length - 1;
+        for(int i = nums.length - 2; i >= 0; i--){
+            if(nums[i] < nums[i + 1]){
+                point = i;
                 break;
             }
         }
 
-        if(br == -1)
-            reverse(nums);
-        else{
-            
-            for(int i = n - 1; i > br; i--){
-                if(nums[i] > nums[br])
-                {
-                    int temp = nums[i];
-                    nums[i] = nums[br];
-                    nums[br] = temp;
+        if(point != nums.length - 1){
+            for(int i = nums.length - 1; i > point; i--){
+                if(nums[i] > nums[point]){
+                    int temp = nums[point];
+                    nums[point] = nums[i];
+                    nums[i] = temp;
                     break;
-                }
+                }   
+                
             }
-
-            reverse(nums,br + 1);
+            reverse(nums, point + 1, nums.length - 1);
+           
+        }else{
+            reverse(nums, 0, nums.length - 1);
         }
     }
 
-    private void reverse(int[] nums, int idx){
-        int n = nums.length;
-        int i = idx, j = n - 1;
+    public void reverse(int[] nums, int startIdx, int endIdx){
+        int i = startIdx, j = endIdx;
         while(i < j){
             int temp = nums[i];
             nums[i] = nums[j];
@@ -37,9 +35,5 @@ class Solution {
             i++;
             j--;
         }
-    }
-
-    private void reverse(int[] nums){
-        reverse(nums,0);
     }
 }
