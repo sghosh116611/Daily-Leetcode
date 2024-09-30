4e11248c7abadab9ffe97e9d6d6df4dfb874a03c
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-09-30 09:38:28 +0530

    Time: 1 ms (18.40%) | Memory: 42.8 MB (88.17%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -1,39 +1,36 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        
-        int point = nums.length - 1;
-        for(int i = nums.length - 2; i >= 0; i--){
+        int n = nums.length;
+        int idx = -1;
+        for(int i = n - 2; i >= 0; i--){
             if(nums[i] < nums[i + 1]){
-                point = i;
+                idx = i;
                 break;
             }
         }
 
-        if(point != nums.length - 1){
-            for(int i = nums.length - 1; i > point; i--){
-                if(nums[i] > nums[point]){
-                    int temp = nums[point];
-                    nums[point] = nums[i];
-                    nums[i] = temp;
+        if(idx != -1){
+            for(int i = n - 1; i > idx; i--)
+                if(nums[i] > nums[idx]){
+                    swap(nums,idx,i);
                     break;
-                }   
-                
+                }
+
+            int i = idx + 1;
+            int j = n - 1;
+            while(i < j){
+                swap(nums,i,j);
+                i++;
+                j--;
             }
-            reverse(nums, point + 1, nums.length - 1);
-           
         }else{
-            reverse(nums, 0, nums.length - 1);
+            Arrays.sort(nums);
         }
     }
 
-    public void reverse(int[] nums, int startIdx, int endIdx){
-        int i = startIdx, j = endIdx;
-        while(i < j){
-            int temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-            i++;
-            j--;
-        }
+    private void swap(int[] nums, int i, int j){
+        int k = nums[i];
+        nums[i] = nums[j];
+        nums[j] = k;
     }
 }
