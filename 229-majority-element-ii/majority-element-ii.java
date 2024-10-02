4f349785844565ef53e69c692d4d3fa6b4bfea19
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-10-02 16:43:30 +0530

    Time: 148 ms (5.70%) | Memory: 45.8 MB (99.91%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -2,43 +2,42 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> result = new ArrayList<>();
 
-        int major1 = -1;
-        int major2 = -1;
-
+        int maj1 = -1;
         int count1 = 0;
+        int maj2 = -1;
         int count2 = 0;
 
         for(int num : nums){
-            if(num == major1)   count1++;
-            else if(num == major2) count2++;
-            else if(count1 == 0){
-                major1 = num;
+            if(num == maj1){
+                count1++;
+            }else if(num == maj2){
+                count2++;
+            }else if(count1 == 0){
+                maj1 = num;
                 count1 = 1;
             }else if(count2 == 0){
-                major2 = num;
+                maj2 = num;
                 count2 = 1;
-            }else{
+            }
+            else{
                 count1--;
                 count2--;
             }
+            System.out.println(maj1 + " " + maj2);
         }
-
+        
         count1 = 0;
         count2 = 0;
-
-        for(int num : nums){
-            if(num == major1) count1++;
-            else if(num == major2) count2++;
+        for(int num: nums){
+            if(num == maj1) count1++;
+            else if(num == maj2) count2++;
         }
 
-        if(count1 > Math.floor(nums.length / 3)){
-            result.add(major1);
-        }
-
-        if(count2 > Math.floor(nums.length / 3)){
-            result.add(major2);
-        }
-
+        if(count1 > nums.length / 3)
+            result.add(maj1);
+        if(count2 > nums.length / 3)
+            result.add(maj2);
+        
         return result;
     }
 }
