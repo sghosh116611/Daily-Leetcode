5cf5aecfbcb91654176aa7d644d033f039d653b6
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-10-03 09:05:49 +0530

    Time: 17 ms (74.74%) | Memory: 43.6 MB (84.46%) - LeetSync

--- 18-4sum/4sum.java
+++ 18-4sum/4sum.java
@@ -1,42 +1,41 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        Arrays.sort(nums);  
-        int n = nums.length;
+        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
+        int n = nums.length;
         for(int i = 0; i < n - 1; i++){
             if(i > 0 && nums[i] == nums[i - 1]) continue;
             for(int j = i + 1; j < n - 2; j++){
                 if(j > i + 1 && nums[j] == nums[j - 1]) continue;
-                int l = j + 1;
-                int r = n - 1;
+                int low = j + 1;
+                int high = n - 1;
 
-                while(l < r){
+                while(low < high){
                     long sum = nums[i];
                     sum += nums[j];
-                    sum += nums[l];
-                    sum += nums[r];
-                    if(sum == target)
-                    {
-                        List<Integer> temp = new ArrayList<>();
-                        temp.add(nums[i]);
-                        temp.add(nums[j]);
-                        temp.add(nums[l]);
-                        temp.add(nums[r]);
-                        result.add(temp);
-                        l++;
-                        r--;
-
-                        while(l < r && nums[l] == nums[l - 1]) l++;
-                        while(l < r && nums[r] == nums[r + 1]) r--;
-                    }else if(sum > target){
-                        r--;
+                    sum += nums[low];
+                    sum += nums[high];
+                    if(sum == target){
+                        List<Integer> res = new ArrayList<>();
+                        res.add(nums[i]);
+                        res.add(nums[j]);
+                        res.add(nums[low]);
+                        res.add(nums[high]);
+                        result.add(res);
+                        low++;
+                        high--;
+                        while(low < high && nums[low - 1] == nums[low])
+                            low++;
+                        while(low < high && nums[high + 1] == nums[high])
+                            high--;
+                    }else if(sum < target){
+                        low++;
                     }else{
-                        l++;
+                        high--;
                     }
                 }
             }
         }
-
         return result;
     }
 }
