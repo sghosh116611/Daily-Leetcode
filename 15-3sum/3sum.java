9036f7366a5100147396fa4d28e0f8321bbbc26a
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-10-16 10:13:56 +0530

    Time: 34 ms (37.70%) | Memory: 52.3 MB (25.00%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,33 +1,34 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
 
-        for(int i = 0; i < nums.length - 1; i++){
-            if(i > 0 && nums[i] == nums[i - 1]) continue;
-            int p = i + 1;
-            int q = nums.length - 1;
-            
-            while(p < q){
-                int sum = nums[i] + nums[p] + nums[q];
+        for(int i = 0; i < nums.length - 2; i++){
+            if(i > 0 && nums[i - 1] == nums[i]) continue;
+            long sum = 0;
+            int low = i + 1;
+            int high = nums.length - 1;
+            while(low < high){
+                sum = nums[i] + nums[low] + nums[high];
                 if(sum == 0){
-                    List<Integer> res = new ArrayList<>();
-                    res.add(nums[i]);
-                    res.add(nums[p]);
-                    res.add(nums[q]);
-                    result.add(res);
-                    while(p < q && nums[p] == nums[p + 1]) p++;
-                    while(p < q && nums[q] == nums[q - 1]) q--;
-                    p++;
-                    q--;
-                }else if(sum > 0){
-                    q--;
-                }else{
-                    p++;
-                }
-            }
+                    List<Integer> row = new ArrayList<>();
+                    row.add(nums[i]);
+                    row.add(nums[low]);
+                    row.add(nums[high]);
+                    low++;
+                    high--;
 
-            
+                    result.add(row);
+
+                    while(low < high && nums[low] == nums[low - 1]) low++;
+                    while(high < nums.length - 1 && high >= 0 && nums[high] == nums[high + 1]) high--;
+                }else if(sum > 0){
+                    high--;
+                }else{
+                    low++;
+                }
+                
+            }
         }
 
         return result;
