841b34c9954311d9533dcb6e60b99fa45bca70a5
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-11-26 15:26:04 +0530

    Time: 7 ms (64.21%) | Memory: 48 MB (28.41%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,24 +1,29 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         HashMap<String,List<String>> map = new HashMap<>();
-        List<List<String>> ans = new ArrayList<>();
-        for(int i = 0; i < strs.length; i++){
-            String sortedString = sortString(strs[i]);
-            if(!map.containsKey(sortedString)){
-                map.put(sortedString,new ArrayList<String>());
+        List<List<String>> result = new ArrayList<>();
+        for(String str : strs){
+            String sortedStr = sortWord(str);
+            if(map.containsKey(sortedStr)){
+                List<String> row = map.get(sortedStr);
+                row.add(str);
+            }else{
+                List<String> words = new ArrayList<>();
+                words.add(str);
+                map.put(sortedStr, words);
             }
-            map.get(sortedString).add(strs[i]);
         }
 
-        for(Map.Entry<String,List<String>> ele:map.entrySet()){
-            ans.add(ele.getValue());
+        for (Map.Entry<String,List<String>> ele : map.entrySet()) {
+            result.add(ele.getValue());
         }
-        return ans;
+
+        return result;
     }
 
-    public String sortString(String s){
-        char word[] = s.toCharArray();
-        Arrays.sort(word);
-        return String.valueOf(word);
+    private String sortWord(String word){
+        char[] chars = word.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 }
