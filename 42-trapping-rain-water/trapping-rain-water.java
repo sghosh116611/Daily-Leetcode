0b28ef53ecd21c1fa08fc671ddf65f012256add5
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-11-26 15:19:08 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (29.81%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,27 +1,26 @@
 class Solution {
     public int trap(int[] height) {
-        int sum = 0;
-        if(height.length == 0)
-            return 0;
+        int left = 0, right = height.length - 1;
 
-        int n = height.length;
-        int left[] = new int[n];
-        int right[] = new int[n];
-
-        left[0] = height[0];
-        right[n - 1] = height[n - 1];
-
-        for(int i = 1; i < n; i++){
-            left[i] = Math.max(left[i - 1],height[i]);
+        int maxLeft = Integer.MIN_VALUE, maxRight = Integer.MIN_VALUE, total = 0;
+        while(left < right){
+            if(height[left] <= height[right]){
+                if(height[left] >= maxLeft){
+                    maxLeft = height[left];
+                }else{
+                    total += maxLeft - height[left];
+                }
+                left++;
+            }else{
+                if(height[right] >= maxRight){
+                    maxRight = height[right];
+                }else{
+                    total += maxRight - height[right];
+                }
+                right--;
+            }
         }
 
-        for(int i = n - 2; i >= 0; i--){
-            right[i] = Math.max(right[i + 1], height[i]);
-        }
-
-        for(int i = 0; i < height.length; i++){
-            sum += Math.min(left[i],right[i]) - height[i];
-        }
-        return sum;
+        return total;
     }
 }
