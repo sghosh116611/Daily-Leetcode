b5fe1febcc1355332360b3e02ec303b1ce11f1f9
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-11-26 17:50:58 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (27.90%) - LeetSync

--- 50-powx-n/powx-n.java
+++ 50-powx-n/powx-n.java
@@ -1,5 +1,15 @@
 class Solution {
     public double myPow(double x, int n) {
-        return Math.pow(x,n);
+        double result = 1.0;
+        long absN = Math.abs((long)n); 
+
+        while (absN != 0) {
+            if ((absN & 1) != 0) 
+                result *= x; 
+            x *= x;           
+            absN >>= 1;       
+        }
+
+        return n < 0 ? 1 / result : result;
     }
 }
