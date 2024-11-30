f451c17fb3e1fd47da36815369a8477eaef0448e
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2024-11-30 09:04:23 +0530

    Time: 6 ms (6.17%) | Memory: 42.1 MB (85.88%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -2,70 +2,54 @@ class Solution {
     public int orangesRotting(int[][] grid) {
         int m = grid.length;
         int n = grid[0].length;
+
         Queue<Pair> queue = new LinkedList<>();
-        int[][] visited = new int[m][n];
-        int rotten = 0,empty = 0,fresh = 0;
-        int mins = 0;
+        int fresh = 0, mins = 0;
 
         for(int i = 0; i < m; i++){
             for(int j = 0; j < n; j++){
-                if(grid[i][j] == 2){
+                if(grid[i][j] == 2)
                     queue.add(new Pair(i,j));
-                }else if(grid[i][j] == 1){
+                else if(grid[i][j] == 1)
                     fresh++;
-                }
             }
         }
 
         if(fresh == 0)
             return 0;
+        
+        int dir[][] = {{1,0},{-1,0},{0,1},{0,-1}};
 
         System.out.println(fresh);
 
         while(!queue.isEmpty()){
-            mins++;
             int size = queue.size();
             while(size-- > 0){
-                Pair cell = queue.poll();
-                int i = cell.i;
-                int j = cell.j;
-
-                    if(i + 1 < m && visited[i + 1][j] != 1 && grid[i + 1][j] == 1){
-                        grid[i + 1][j] = 2;
-                        queue.add(new Pair(i + 1,j));
-                        
-                fresh--;
-                    }
-
-                    if(i - 1 >= 0 && visited[i - 1][j] != 1 && grid[i - 1][j] == 1){
-                        grid[i - 1][j] = 2;
-                        queue.add(new Pair(i - 1,j));
-                        
-                fresh--;
-                    }
-
-                    if(j + 1 < n && visited[i][j + 1] != 1 && grid[i][j + 1] == 1){
-                        grid[i][j + 1] = 2;
-                        queue.add(new Pair(i,j + 1));
-                        
-                fresh--;
-                    }
-
-                    if(j - 1 >= 0 && visited[i][j - 1] != 1 && grid[i][j - 1] == 1){
-                        grid[i][j - 1] = 2;
-                        queue.add(new Pair(i,j - 1));
-                        
-                fresh--;
-                    }
-                
-                System.out.println(fresh);
-            }
+                Pair pair = queue.poll();
+                int i = pair.i;
+                int j = pair.j;
             
+                for(int k = 0; k <= 3; k++){
+                    int x1 = i + dir[k][0];
+                    int y1 = j + dir[k][1];
+
+                    if(x1 >= 0 && x1 < m && y1 >= 0 && y1 < n && grid[x1][y1] == 1){
+                        fresh--;
+                        queue.add(new Pair(x1,y1));
+                        grid[x1][y1] = 2;
+                    }   
+                }
+            }
+            mins++;
         }
+
+        System.out.println(fresh);
+
         return fresh == 0 ? mins - 1 : -1;
+        
     }
 
-    class Pair{
+    private class Pair{
         int i;
         int j;
 
