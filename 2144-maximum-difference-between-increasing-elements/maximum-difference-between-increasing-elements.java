133d0bb1be4567647930a9744058d37e3de99a37
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2025-06-16 10:14:48 +0530

    Time: 1 ms (57.39%) | Memory: 41.7 MB (95.13%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -2,18 +2,23 @@ class Solution {
     public int maximumDifference(int[] nums) {
         int n = nums.length;
         int[] maxElementToRight = new int[n];
-        int max = 0;
-
-        for(int i = n - 1; i >= 0; i--){
-            max = Math.max(nums[i],max);
-            maxElementToRight[i] = max;
-        }
-
+        int min = nums[0];
         int diff = 0;
-        for(int i = 0; i < n; i++){
-            diff = Math.max(diff,maxElementToRight[i] - nums[i]);
-        }
 
+        for(int i = 1; i < n; i++){
+            min = Math.min(min,nums[i - 1]);
+            if(min < nums[i]) diff = Math.max(diff,nums[i] - min);
+        }
+        
         return diff == 0 ? - 1 : diff;
+        // for(int i = n - 1; i >= 0; i--){
+        //     max = Math.max(nums[i],max);
+        //     maxElementToRight[i] = max;
+        // }
+
+        
+        // for(int i = 0; i < n; i++){
+        //     diff = Math.max(diff,maxElementToRight[i] - nums[i]);
+        // }
     }
 }
