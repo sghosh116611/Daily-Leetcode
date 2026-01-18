4bd5aecdd3c30ff90102249aefad586d04d9f003
Soumojit Ghosh <51120319+sghosh116611@users.noreply.github.com>
2026-01-18 10:46:24 +0530

    Time: 2 ms (95.03%) | Memory: 45.9 MB (26.80%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,28 +1,27 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        func(0,candidates,target,ans, new ArrayList<>());
-        return ans;
+        List<List<Integer>> res = new ArrayList<>();
+        func(candidates,target,res,new ArrayList<>(),0);
+        return res;
     }
-
-    private void func(int idx, int nums[], int target, List<List<Integer>> ans, 
-    List<Integer> ds){
-
-        // Base condition
-        if(idx == nums.length){
-            if(target == 0){
-                ans.add(new ArrayList<>(ds));
-            }
+    
+    private void func(int[] nums, int k, List<List<Integer>> res, List<Integer> ds, int idx) {
+        
+        if(idx == nums.length) {
+            if(k == 0)
+                res.add(new ArrayList<>(ds));
             return;
         }
-        // Take
-        if(nums[idx] <= target){
+
+        if(nums[idx] <= k) {
             ds.add(nums[idx]);
-            func(idx,nums,target - nums[idx],ans,ds);
+            int take = k - nums[idx];
+            func(nums,take,res,ds,idx);
             ds.remove(ds.size() - 1);
         }
 
-        // Not take
-        func(idx + 1, nums, target, ans, ds);
+        int notTake = k;
+        func(nums,notTake,res,ds,idx + 1);
     }
+
 }
